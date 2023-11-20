0dc4a7d8b821623c0b0e9e087cddf5f78e2ad0d1
harika-majji <145294690+harika-majji@users.noreply.github.com>
2023-11-19 20:52:59 -0600

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,16 +1,30 @@
 class Chatbox {
+
+    
     constructor() {
         this.args = {          
             chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
+            sendButton: document.querySelector('.send__button'),
+            chatBotButton: document.querySelector('.chat_bot'),
+            quizBotButton: document.querySelector('.quiz_bot')
+
         }
 
         this.state = false;
         this.messages = [];
+        this.evaluate = false;
+
+        this.currentQuestionIndex = 0;
+        this.score = 0;
+
+        // List of questions for the quiz
+        this.quizQuestions = [];
+        this.quizAnswers = [];
     }
 
+    
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const {openButton, chatBox, sendButton, quizBotButton} = this.args;
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
 
         const node = chatBox.querySelector('input');
@@ -19,46 +33,105 @@ class Chatbox {
                 this.onSendButton(chatBox)
             }
         })
+
+        // this.displayQuizQuestion(chatBox);
     }
-    
+    displayQuizQuestion(chatbox) {
+        console.log("In display")
+        const quizQuestion = this.quizQuestions[this.currentQuestionIndex];
+        const quizMessage = { name: "Bot", message: `Quiz: ${quizQuestion}` };
+        this.messages.push(quizMessage);
+        this.updateChatText(chatbox);
+    }
+
     onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
-            return;
+        if(this.evaluate === true){
+            console.log("answer check");
+            var textField = chatbox.querySelector('input');
+            let userAnswer = textField.value;
+            if (userAnswer === "") {
+                return;
+            }
+    
+            let userMessage = { name: "User", message: userAnswer };
+            this.messages.push(userMessage);
+            
+            console.log(userAnswer)
+            console.log(this.quizAnswers[this.currentQuestionIndex])
+            // Check the user's answer if needed
+            if(userAnswer === this.quizAnswers[this.currentQuestionIndex]){
+                this.score++
+            }
+               
+    
+            // Move to the next quiz question
+            this.currentQuestionIndex++
+    
+            if (this.currentQuestionIndex < this.quizQuestions.length) {
+                // Display the next quiz question
+                this.displayQuizQuestion(chatbox);
+            } else {
+
+
+                let scoreMessage = { name: "Bot", message: "Quiz completed. Your score is "+this.score+" out of "+this.quizQuestions.length };
+                this.messages.push(scoreMessage);
+                this.updateChatText(chatbox);
+                // Display a message when all quiz questions are answered
+                let finalMessage = { name: "Bot", message: "Thanks for participating!" };
+                this.messages.push(finalMessage);
+                this.updateChatText(chatbox);
+                this.evaluate = false;
+            }
+    
+            // Clear the user input
+            textField.value = '';
         }
+        else{
+            console.log("Api call");
+            var textField = chatbox.querySelector('input')
+            let text1 = textField.value
+            if (text1 === "") {
+                return;
+            }
 
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
-        this.updateChatText(chatbox)
-        const timeoutInMilliseconds = 300000;
-        fetch($SCRIPT_ROOT + '/generateChat', {
-            method: 'POST',
-            body: JSON.stringify({ question: text1 }),
-            mode: 'cors',
-            timeout: timeoutInMilliseconds,
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Sam", message: r.answer + " --- " + r.citation };
-            this.messages.push(msg2);
+            let msg1 = { name: "User", message: text1 }
+            this.messages.push(msg1)
             this.updateChatText(chatbox)
             textField.value = ''
+            const timeoutInMilliseconds = 300000;
+            fetch($SCRIPT_ROOT + '/generateQuiz', {
+                method: 'POST',
+                body: JSON.stringify({ question: text1 }),
+                mode: 'cors',
+                timeout: timeoutInMilliseconds,
+                headers: {
+                'Content-Type': 'application/json'
+                },
+            })
+            .then(r => r.json())
+            .then(r => {
+                this.quizAnswers = r.answers
+                this.quizQuestions = r.questions
+                this.evaluate = true
+                this.displayQuizQuestion(chatbox)
+                // let msg2 = { name: "Bot", message: r.answer + " --- " + r.citation };
+                // this.messages.push(msg2);
+                // this.updateChatText(chatbox)
+                textField.value = ''
+               
 
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
+            }).catch((error) => {
+                console.error('Error:', error);
+                this.updateChatText(chatbox)
+                textField.value = ''
+            });
+        }
     }
 
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
+            if (item.name === "Bot")
             {
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
@@ -72,6 +145,7 @@ class Chatbox {
         chatmessage.innerHTML = html;
     }
 }
+
 
 
 const chatbox = new Chatbox();
